58fdf33628b6a0346b9707625647d166babdc5ae
cbk914 <cbk914@riseup.net>
2020-11-10 23:46:10 +0100

    Add toolbox check by default

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 clear
 git pull
-./install-dependencies.sh
+sudo ./install-dependencies.sh
 python3 jok3r.py toolbox --update-all --auto --check
 #python3 jok3r.py toolbox --install-all --auto --check
 python3 jok3r.py toolbox --show-all
